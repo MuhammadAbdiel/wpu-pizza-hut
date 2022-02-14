d4601be6a2cbf30c6ad346466d6cd8a3c221336f
MuhammadAbdiel <abdielfirdie@gmail.com>
2022-02-14 14:50:13 +0700

    Memperbaiki bagian jQuery yaitu di bagian sintax html yang awalnya memakai tanda kutip diubah menjadi memakai backtick

--- js/script.js
+++ js/script.js
@@ -4,7 +4,19 @@
 //     $.getJSON('data/pizza.json', function (hasil) {
 //         let menu = hasil.menu;
 //         $.each(menu, function (i, data) {
-//             $("#daftar-menu").append(`<div class="col-md-4"><div class="card mb-3"><img src="img/menu/${data.gambar}" class="card-img-top"><div class="card-body"><h5 class="card-title">${data.nama}</h5><p class="card-text">${data.deskripsi}</p><h5 class="card-title">${data.harga}</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>`);
+//             $("#daftar-menu").append(`
+//                 <div class="col-md-4">
+//                     <div class="card mb-3">
+//                         <img src="img/menu/${data.gambar}" class="card-img-top">
+//                         <div class="card-body">
+//                             <h5 class="card-title">${data.nama}</h5>
+//                             <p class="card-text">${data.deskripsi}</p>
+//                             <h5 class="card-title">${data.harga}</h5>
+//                             <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+//                         </div>
+//                     </div>
+//                 </div>
+//             `);
 //         });
 //     });
 // }
@@ -29,7 +41,19 @@
 
 //         $.each(menu, function (i, data) {
 //             if (data.kategori == kategori.toLowerCase()) {
-//                 content += `<div class="col-md-4"><div class="card mb-3"><img src="img/menu/${data.gambar}" class="card-img-top"><div class="card-body"><h5 class="card-title">${data.nama}</h5><p class="card-text">${data.deskripsi}</p><h5 class="card-title">${data.harga}</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>`;
+//                 content += `
+//                     <div class="col-md-4">
+//                         <div class="card mb-3">
+//                         <img src="img/menu/${data.gambar}" class="card-img-top">
+//                             <div class="card-body">
+//                                 <h5 class="card-title">${data.nama}</h5>
+//                                 <p class="card-text">${data.deskripsi}</p>
+//                                 <h5 class="card-title">${data.harga}</h5>
+//                                 <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 `;
 //             }
 //         });
 
